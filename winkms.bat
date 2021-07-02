2800ef13a8ebbbb31ea933c9ce0b2871c1672ea0
whitebear13579 <68762923+whitebear13579@users.noreply.github.com>
2021-07-02 14:58:34 +0800

    Add files via upload

---
+++ winkms.bat
@@ -0,0 +1,9 @@
+@echo off
+echo 歡迎使用，Windows 啟用程式_作者:whitebear13579
+slmgr /upk
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms.v0v.bid
+slmgr /ato
+echo 您的Windows 已成功啟用
+echo windows啟用工具版本:v1.3 適用於Windows10和windows11
+echo. &pause
